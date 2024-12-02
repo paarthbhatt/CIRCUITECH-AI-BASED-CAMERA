68d33265bba9c8ed74597fb98a95b77a36b653f8
Parth Bhatt <paarthbhatt37@gmail.com>
2024-12-02 20:22:15 +0530

    git add . git push

--- api.php
+++ api.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $image = $_FILES['image']['tmp_name'];
 
